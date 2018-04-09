2957b68ddf666e4528995d2bb3142a4c1badd9d7
Dmitriy Halikov <dmitriy.halikov@webpp.ru>
2018-04-09 18:22:01 +0300

    WIP: Шаблонизация данных

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var dummyData = [];
+var fragment = document.createDocumentFragment();
 
 var generateDummy = function() {
     var dummy_quantity = 8;
@@ -33,7 +34,7 @@ var generateDummy = function() {
         'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
     ];
 
-    var location
+    var location;
 
     for (var i = 0; i < dummy_quantity; i++) {
 
@@ -49,7 +50,7 @@ var generateDummy = function() {
         item['offer']['checkout'] = getRandomItem(offer_checkout);
         item['offer']['features'] = getRanfomFeatures(offer_features);
         item['offer']['description'] = '';
-        item['offer']['photos']   = [];
+        item['offer']['photos']   = getRanfomPhotos(offer_photos);
 
         item['location']['x']     = getRandomRange(300, 900);
         item['location']['y']     = getRandomRange(150, 500);
@@ -86,13 +87,67 @@ var generateDummy = function() {
             return features;
         };
 
+        function getRanfomPhotos(arr) {
+            var ctr = arr.length, temp, index;
 
-
+            while (ctr > 0) {
+                index = Math.floor(Math.random() * ctr);
+                ctr--;
+                temp = arr[ctr];
+                arr[ctr] = arr[index];
+                arr[index] = temp;
+            }
+            return arr;
+        };
 
         dummyData.push(item);
     }
+};
 
-    console.log(dummyData)
+var renderPins = function(data) {
+
+    var pinItemTemplate = document.querySelector('#map-pin')
+        .content
+        .querySelector('.map__card');
+
+    var pinItem = pinItemTemplate.cloneNode(true);
+
+    pinItem.querySelector('.popup__avatar').src = data.author.avatar;
+    pinItem.querySelector('.popup__avatar').alt = data.offer.title;
+    pinItem.querySelector('.popup__title').textContent = data.offer.title;
+    pinItem.querySelector('.popup__text--address').textContent = data.offer.address;
+    pinItem.querySelector('.popup__text--price').textContent = data.offer.price + ' ₽/ночь';
+    pinItem.querySelector('.popup__type').textContent = data.offer.type;
+
+// <article class="map__card popup">
+//   <img src="img/avatars/user01.png" class="popup__avatar" width="70" height="70" alt="Аватар пользователя">
+//   <button type="button" class="popup__close">Закрыть</button>
+//   <h3 class="popup__title">Уютное гнездышко для молодоженов</h3>
+//   <p class="popup__text popup__text--address">102-0082 Tōkyō-to, Chiyoda-ku, Ichibanchō, 14−3</p>
+//   <p class="popup__text popup__text--price">5200&#x20bd;<span>/ночь</span></p>
+//   <h4 class="popup__type">Квартира</h4>
+//   <p class="popup__text popup__text--capacity">2 комнаты для 3 гостей</p>
+//   <p class="popup__text popup__text--time">Заезд после 14:00, выезд до 10:00</p>
+//   <ul class="popup__features">
+//     <li class="popup__feature popup__feature--wifi"></li>
+//     <li class="popup__feature popup__feature--dishwasher"></li>
+//     <li class="popup__feature popup__feature--parking"></li>
+//     <li class="popup__feature popup__feature--washer"></li>
+//     <li class="popup__feature popup__feature--elevator"></li>
+//     <li class="popup__feature popup__feature--conditioner"></li>
+//   </ul>
+//   <p class="popup__description">Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.</p>
+//   <div class="popup__photos">
+//     <img src="" class="popup__photo" width="45" height="40" alt="Фотография жилья">
+//   </div>
+// </article>
+    return pinItem;
 };
 
 generateDummy();
+
+for (var i = 0; i < dummyData.length; i++) {
+    fragment.appendChild(renderPins(dummyData[i]))
+}
+
+document.querySelector('.map__pins').appendChild(fragment);
